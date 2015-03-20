6fe24991a2922aa9fde63bbc4ba5a438b8a5997c
TT <caveguy@qq.com>
2015-03-20 11:01:01 +0800

    modified alpha_soft
    	modified:   platform/alpha_soft.cpp

--- platform/alpha_soft.cpp
+++ platform/alpha_soft.cpp
@@ -4,6 +4,9 @@
 #define MAX_ALPHA 255
 #define MIN_ALPHA 0
 
+
+
+
 void Render_img_to_img(image * dst, image * src, int src_x, int src_y, int cp_width, int cp_height, int dst_x, int dst_y)
 {
 //	ProcArea(dst, src, src_x, src_y, cp_width, cp_height, dst_x, dst_y);
@@ -14,18 +17,23 @@ void Render_img_to_img(image * dst, image * src, int src_x, int src_y, int cp_wi
 
 	int x;
 	int y;
+	int alpha;
 	for (y = 0; y < cp_height; y++)
 	{
 		for (x = 0; x < cp_width; x++)
 		{
-			IMG_PIX * dst_pix = ((IMG_PIX *) dst->pSrcBuffer + (y + dst_y) * dst->u32Width + x + dst_x);
-			IMG_PIX * src_pix = ((IMG_PIX *) src->pSrcBuffer + (y + src_y) * src->u32Width + x + src_x);
-			int alpha = src_pix->u8Alpha*src->transp/100;
-			dst_pix->u8Red =  (dst_pix->u8Red * (MAX_ALPHA - alpha) + src_pix->u8Red * alpha) / MAX_ALPHA;
-			dst_pix->u8Green = (dst_pix->u8Green * (MAX_ALPHA - alpha) + src_pix->u8Green * alpha) / MAX_ALPHA;
-			dst_pix->u8Blue =  (dst_pix->u8Blue * (MAX_ALPHA - alpha) + src_pix->u8Blue * alpha) / MAX_ALPHA;
-			dst_pix->u8Alpha = (dst_pix->u8Alpha>src_pix->u8Alpha?dst_pix->u8Alpha:src_pix->u8Alpha);
+			Color32 * dst_pix = ((Color32 *) dst->pSrcBuffer + (y + dst_y) * dst->u32Width + x + dst_x);
+			Color32 * src_pix = ((Color32 *) src->pSrcBuffer + (y + src_y) * src->u32Width + x + src_x);
+			 alpha = src_pix->a*src->transp/100;
+			dst_pix->r =  (dst_pix->r * (MAX_ALPHA - alpha) + src_pix->r * alpha) / MAX_ALPHA;
+			dst_pix->g = (dst_pix->g * (MAX_ALPHA - alpha) + src_pix->g * alpha) / MAX_ALPHA;
+			dst_pix->b =  (dst_pix->b * (MAX_ALPHA - alpha) + src_pix->b * alpha) / MAX_ALPHA;
+			dst_pix->a = (dst_pix->a>src_pix->a?dst_pix->a:src_pix->a);
 			//((S_DRVBLT_ARGB8 *) dst->pSrcBuffer + (dst->y + dst_y) * dst->u32Width + dst->x + dst_x)->u8Alpha = 255;
 		}
 	}
+
+
+
+
 }
